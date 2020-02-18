8aa603d201567fef810a741ab4d5d61e27539d49
Marcus Marshall, Jr <mmarshall.inbox@gmail.com>
2020-02-18 16:12:26 -0600

    Created a temp screen for us to navigate to our activities

--- LendaHandApp/app/src/main/java/com/example/lendahand/MainActivity.java
+++ LendaHandApp/app/src/main/java/com/example/lendahand/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.lendahand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //STEP 1: Add reference to button using R.id
+        MaterialButton signupVolunteer = findViewById(R.id.signup_a_volunteer);
+
+        //STEP 2: Set onClickListener for YOUR button
+        signupVolunteer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //STEP 3: Create Intent for your class
+                Intent volunteerSignupScreen = new Intent(v.getContext(), Volunteer_Signup.class);
+                //STEP 4: Start your Activity
+                startActivity(volunteerSignupScreen);
+            }
+        });
+
     }
+
+
+
 }
