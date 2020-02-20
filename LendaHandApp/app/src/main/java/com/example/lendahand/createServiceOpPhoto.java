6217fca922caebc977f90377dbbdae761f56dfc5
stklumph <stklumph@crimson.ua.edu>
2020-02-19 20:00:10 -0600

    Updated more xml files and added class files

--- LendaHandApp/app/src/main/java/com/example/lendahand/createServiceOpPhoto.java
+++ LendaHandApp/app/src/main/java/com/example/lendahand/createServiceOpPhoto.java
@@ -8,21 +8,21 @@ import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 
-public class createServiceOpDateTime extends AppCompatActivity {
+public class createServiceOpPhoto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_service_op_date_time);
+        setContentView(R.layout.activity_create_service_op_photo);
         //STEP 1: Add reference to button using R.id
-        MaterialButton createServiceOpCatReq = findViewById(R.id.btnServiceOpDateTimeNext);
+        MaterialButton createServiceOpPhoto= findViewById(R.id.btnServiceOpPhotoNext);
 
         //STEP 2: Set onClickListener for YOUR button
-        createServiceOpCatReq.setOnClickListener(new View.OnClickListener() {
+        createServiceOpPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //STEP 3: Create Intent for your class
-                Intent createServiceOpScreen = new Intent(v.getContext(), createServiceOpCatReq.class);
+                Intent createServiceOpScreen = new Intent(v.getContext(), MainActivity.class);
                 //STEP 4: Start your Activity
                 startActivity(createServiceOpScreen);
             }
