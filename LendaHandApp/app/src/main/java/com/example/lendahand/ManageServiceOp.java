1ce52592d630b2fa910cb1949f969e8916365d94
stklumph <stklumph@crimson.ua.edu>
2020-03-05 15:40:15 -0600

    updated screens to have input checking

--- LendaHandApp/app/src/main/java/com/example/lendahand/ManageServiceOp.java
+++ LendaHandApp/app/src/main/java/com/example/lendahand/ManageServiceOp.java
@@ -2,7 +2,13 @@ package com.example.lendahand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ManageServiceOp extends AppCompatActivity {
 
@@ -10,5 +16,54 @@ public class ManageServiceOp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_service_op);
+
+        Intent intent = getIntent();
+        final ServiceOpportunity currentServiceOp = (ServiceOpportunity) intent.getSerializableExtra("CurrentServiceOp");
+
+        final TextInputEditText txtServiceOpName = findViewById(R.id.txtEditServiceOpName);
+        final TextInputEditText txtServiceOpSub = findViewById(R.id.txtEditServiceOpSub);
+        final TextInputEditText txtServiceOpDesc = findViewById(R.id.txtEditServiceOpDesc);
+        final TextInputEditText txtServiceOpContactName = findViewById(R.id.txtEditServiceOpContactName);
+        final TextInputEditText txtServiceOpContactEmail = findViewById(R.id.txtEditServiceOpContactEmail);
+        final TextInputEditText txtServiceOpContactPhone = findViewById(R.id.txtEditServiceOpContactPhone);
+
+        final Switch swtchServiceOpRepeat = findViewById(R.id.swtchEditServiceOpRepeat);
+        final TextInputEditText txtServiceOpDate = findViewById(R.id.txtEditServiceOpDate);
+        final TextInputEditText txtServiceOpTime = findViewById(R.id.txtEditServiceOpTime);
+        final TextInputEditText txtServiceOpCutoffDate = findViewById(R.id.txtEditServiceOpCutoffDate);
+        final TextInputEditText txtServiceOpCutoffTime = findViewById(R.id.txtEditServiceOpCutoffTime);
+        final TextInputEditText txtServiceOpLocation = findViewById(R.id.txtEditServiceOpLocation);
+
+        final TextInputEditText txtAgeReq = findViewById(R.id.txtEditAgeReq);
+        final TextInputEditText txtAdditionalReq = findViewById(R.id.txtEditAdditionalReq);
+
+        txtServiceOpName.setText(currentServiceOp.getOpName());
+        txtServiceOpSub.setText(currentServiceOp.getOpSubtitle());
+        txtServiceOpDesc.setText(currentServiceOp.getOpDescription());
+        txtServiceOpContactName.setText(currentServiceOp.getOpContactName());
+        txtServiceOpContactEmail.setText(currentServiceOp.getOpContactEmail());
+        txtServiceOpContactPhone.setText(currentServiceOp.getOpContactPhone());
+
+        swtchServiceOpRepeat.setChecked(currentServiceOp.getOpRepeat());
+        txtServiceOpDate.setText(currentServiceOp.getOpDate());
+        txtServiceOpTime.setText(currentServiceOp.getOpTime());
+        txtServiceOpCutoffDate.setText(currentServiceOp.getOpCutoffDate());
+        txtServiceOpCutoffTime.setText(currentServiceOp.getOpCutoffTime());
+        txtServiceOpLocation.setText(currentServiceOp.getOpLocation());
+        txtAgeReq.setText(currentServiceOp.getOpAgeReq());
+        txtAdditionalReq.setText(currentServiceOp.getOpAdditionalReq());
+
+        //STEP 1: Add reference to button using R.id
+        MaterialButton createServiceOpPhoto = findViewById(R.id.btnMngServiceOpFinish);
+        //STEP 2: Set onClickListener for YOUR button
+        createServiceOpPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //STEP 3: Create Intent for your class
+                Intent createServiceOpScreen = new Intent(v.getContext(), MainActivity.class);
+                //STEP 4: Start your Activity
+                startActivity(createServiceOpScreen);
+            }
+        });
     }
 }
--- LendaHandApp/app/src/main/java/com/example/lendahand/createServiceOpCatReq.java
+++ LendaHandApp/app/src/main/java/com/example/lendahand/createServiceOpCatReq.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.amazonaws.util.StringUtils;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class createServiceOpCatReq extends AppCompatActivity {
 
@@ -14,6 +17,14 @@ public class createServiceOpCatReq extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_service_op_cat_req);
+
+        Intent intent = getIntent();
+        final ServiceOpportunity newServiceOp = (ServiceOpportunity) intent.getSerializableExtra("ServiceOp");
+
+        final TextInputEditText txtAgeReq = findViewById(R.id.txtAgeReq);
+        final TextInputEditText txtAdditionalReq = findViewById(R.id.txtAdditionalReq);
+
+
         //STEP 1: Add reference to button using R.id
         MaterialButton createServiceOpPhoto = findViewById(R.id.btnServiceOpCatReqNext);
 
@@ -21,10 +32,33 @@ public class createServiceOpCatReq extends AppCompatActivity {
         createServiceOpPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //STEP 3: Create Intent for your class
-                Intent createServiceOpScreen = new Intent(v.getContext(), createServiceOpPhoto.class);
-                //STEP 4: Start your Activity
-                startActivity(createServiceOpScreen);
+                String opAgeReq = txtAgeReq.getText().toString().trim();
+                String opAdditionalReq = txtAdditionalReq.getText().toString().trim();
+
+                //Do input checking
+                InputChecker inputChecker = new InputChecker();
+                String error = "";
+
+                error += inputChecker.isBlank(opAgeReq, "Service Opportunity Age Requirement");
+
+                if (StringUtils.isBlank(error)) {
+                    newServiceOp.setOpAgeReq(opAgeReq);
+                    newServiceOp.setOpAdditionalReq(opAdditionalReq);
+
+
+                    //STEP 3: Create Intent for your class
+                    Intent createServiceOpScreen = new Intent(v.getContext(), createServiceOpPhoto.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("ServiceOp", newServiceOp);
+                    createServiceOpScreen.putExtras(bundle);
+                    //STEP 4: Start your Activity
+                    startActivityForResult(createServiceOpScreen, 0);
+                }
+
+                else{
+                    Toast.makeText(v.getContext(), error, Toast.LENGTH_LONG);
+                }
+
             }
         });
     }
