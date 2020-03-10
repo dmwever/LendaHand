b76b2b4013105e55fe2625e97977af9053d47cb9
stklumph <stklumph@crimson.ua.edu>
2020-03-10 15:57:15 -0500

    updated build and others

---
+++ LendaHandApp/app/src/main/java/com/example/lendahand/DisplayServiceOpportunity.java
@@ -0,0 +1,14 @@
+package com.example.lendahand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayServiceOpportunity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_service_opportunity);
+    }
+}
