79c43f5a6abb644d49187cbc584070705be38db7
dmwev <dmwever@crimson.ua.edu>
2020-03-10 17:15:40 -0500

    Add volunteer to database implemented

--- LendaHandApp/app/src/main/java/com/example/lendahand/Database.java
+++ LendaHandApp/app/src/main/java/com/example/lendahand/Database.java
@@ -3,28 +3,57 @@ package com.example.lendahand;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Database extends AppCompatActivity {
 
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    public void test() {
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
-
-        Log.i("createStorage", "Success");
-        myRef.setValue("Hello, World!");
+    public void init() {
+        db = FirebaseFirestore.getInstance();
     }
 
+    public void addVolunteer (Volunteer newVolunteer) {
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("fName", newVolunteer.getFirstName());
+        user.put("lName", newVolunteer.getLastName());
+        user.put("id", newVolunteer.getEmail());
+        user.put("pNum", newVolunteer.getPhone());
+        user.put("DoB", newVolunteer.getDateOfBirth());
+
+// Add a new document with a generated ID
+        db.collection("volunteers")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("init", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("init", "Error adding document", e);
+                    }
+                });
+    }
 
 }
