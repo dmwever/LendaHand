195ab35cf31f25b0d49a2cb484f3fde331771244
stklumph <stklumph@crimson.ua.edu>
2020-04-01 15:04:05 -0500

    added some junit tests for Service Ops

--- LendaHandApp/app/src/main/java/com/example/lendahand/DisplayServiceOpportunity.java
+++ LendaHandApp/app/src/main/java/com/example/lendahand/DisplayServiceOpportunity.java
@@ -5,10 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DisplayServiceOpportunity extends AppCompatActivity {
+    ServiceOpportunity serviceOp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_service_opportunity);
+
+        Bundle bundle = getIntent().getExtras();
+        String ID = bundle.getString("ID");
+
+        final Database db = new Database();
+        db.init();
+        serviceOp = db.getService(ID);
+        if(serviceOp == null){
+            System.out.println("FAILURE");
+        }
     }
 }
